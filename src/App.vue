dfc9a73029e01fb54b425ed012b63c2438b2350b
isaac Groisman <grisman13@hotmail.com>
2023-02-28 21:35:17 +0200

    updated loading animation once  when empty

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@
 <script>
 import SplashScreen from "./components/SplashScreen.vue";
 import { defineAsyncComponent } from "vue";
+import store from "./store.js";
 
 export default {
   components: {
@@ -19,6 +20,11 @@ export default {
     Home: defineAsyncComponent(
       () =>
         new Promise((resolve) => {
+          const savedCurrency = JSON.parse(localStorage.getItem("currency"));
+          //console.log(console.log(store.state.currency));
+          if(savedCurrency == null){
+            localStorage.setItem("currency", JSON.stringify({symbol: store.state.currency, class: 'fa fa-dollar-sign'}));
+          }
           setTimeout(() => {
             resolve(import("./components/Home.vue"));
           }, 2500);
